b0e3895656c3a402147812edcf40e990a7f8ac59
imanyousefi13671988-eng <imanyousefi13671988@gmail.com>
2026-07-27 20:43:21 +0330

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,37 @@
+const envelope = document.getElementById("envelope");
+const card = document.getElementById("card");
+const cardScreen = document.getElementById("cardScreen");
+const music = document.getElementById("music");
+const musicBtn = document.getElementById("musicBtn");
 
+let started = false;
+
+envelope.addEventListener("click", () => {
+
+    if(started) return;
+
+    started = true;
+
+    music.currentTime = 4;
+
+    music.play();
+
+    envelope.classList.add("open");
+
+    setTimeout(()=>{
+
+        cardScreen.classList.add("show");
+
+        card.classList.add("show");
+
+    },2800);
+
+});
+
+musicBtn.addEventListener("click",()=>{
+
+    if(music.paused){
+
+        music.play();
+
+        musicBtn.innerHTML="
